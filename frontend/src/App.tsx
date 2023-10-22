3dc115192b7b5d6d90b2c06a80860107bc0f991a
Paul Christian Rueda <chris14rueda@gmail.com>
2023-10-22 20:32:29 +0800

    Add react typescript initial files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Manager from './components/Manager';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Manager />
+    </div>
+  );
+}
+
+export default App;
