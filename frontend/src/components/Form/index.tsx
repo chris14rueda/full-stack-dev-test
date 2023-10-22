b8c35096185ea634595ffcae0a254fd7dc9d8f1b
Paul Christian Rueda <chris14rueda@gmail.com>
2023-10-22 22:51:31 +0800

    Focus title field after adding task

--- frontend/src/components/Form/index.tsx
+++ frontend/src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Task } from '../../shared/hooks/useTasks';
 import Loader from '../Loader';
 import Styles from './Form.module.scss';
@@ -9,23 +9,25 @@ interface FormProps {
 }
 
 const Form = ({ onAdd, isAdding }: FormProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [title, setTitle] = useState<string>('');
 
   const handleAdd = () => {
     onAdd({ title });
     setTitle('');
+    inputRef.current?.focus();
   };
 
   return (
     <div className={Styles.container}>
       <input
+        ref={inputRef}
         type={'text'}
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         onKeyPress={(e) => e.key === 'Enter' && handleAdd()}
         placeholder="Enter a task"
         className={Styles.input}
-        disabled={isAdding}
       />
       <button onClick={handleAdd} className={Styles.btn} disabled={!title || isAdding}>
         {isAdding ? <Loader /> : 'Add'}
